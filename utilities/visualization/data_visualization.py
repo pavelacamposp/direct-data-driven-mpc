880b9f10dcdae7d93e04d81de2fb558723bf6e2d
Pável A. Campos-Peña <pcamposp@uni.pe>
2025-02-11 09:47:15 -0500

    Add support for online input-output measurement animation
    - Refactor input-output plot animation functions to support moving
      the initial measurement period highlight with the latest data.
      This represents continuous input-output measurement updates,
      which are typical of nonlinear data-driven control systems.
    - Rename rectangle boundary line variables for clarity:
        - `rect_lines` to `right_rect_lines`.
        - `u_rect_lines`, `y_rect_lines` to `u_right_rect_lines`,
          `y_right_rect_lines`.

--- utilities/visualization/data_visualization.py
+++ utilities/visualization/data_visualization.py
@@ -437,6 +437,7 @@ def plot_input_output_animation(
     setpoints_line_params: dict[str, Any] = {},
     bounds_line_params: dict[str, Any] = {},
     initial_steps: Optional[int] = None,
+    continuous_updates: bool = False,
     initial_excitation_text: str = "Init. Excitation",
     initial_measurement_text: str = "Init. Measurement",
     control_text: str = "Data-Driven MPC",
@@ -516,6 +517,9 @@ def plot_input_output_animation(
             characterization of the system. This will highlight the initial
             measurement period in the plot. If `None`, no special highlighting
             will be applied.
+        continuous_updates (bool): Whether the initial measurement period
+            highlight should move with the latest data to represent continuous
+            input-output measurement updates.
         initial_excitation_text (str): Label text to display over the initial
             excitation period of the input plots. Default is
             "Init. Excitation".
@@ -587,11 +591,13 @@ def plot_input_output_animation(
 
     # Define initial measurement rectangles and texts lists
     u_rects: List[Rectangle] = []
-    u_rect_lines: List[Line2D] = []
+    u_right_rect_lines: List[Line2D] = []
+    u_left_rect_lines: List[Line2D] = []
     u_init_texts: List[Text] = []
     u_control_texts: List[Text] = []
     y_rects: List[Rectangle] = []
-    y_rect_lines: List[Line2D] = []
+    y_right_rect_lines: List[Line2D] = []
+    y_left_rect_lines: List[Line2D] = []
     y_init_texts: List[Text] = []
     y_control_texts: List[Text] = []
 
@@ -618,12 +624,14 @@ def plot_input_output_animation(
                                   legend_params=legend_params,
                                   lines=u_lines,
                                   rects=u_rects,
-                                  rect_lines=u_rect_lines,
+                                  right_rect_lines=u_right_rect_lines,
+                                  left_rect_lines=u_left_rect_lines,
                                   init_texts=u_init_texts,
                                   control_texts=u_control_texts,
                                   y_axis_centers=u_y_axis_centers,
                                   bounds=u_bounds,
                                   initial_steps=initial_steps,
+                                  continuous_updates=continuous_updates,
                                   legend_loc='upper right')
     
     # Initialize output plot elements
@@ -645,12 +653,14 @@ def plot_input_output_animation(
                                   legend_params=legend_params,
                                   lines=y_lines,
                                   rects=y_rects,
-                                  rect_lines=y_rect_lines,
+                                  right_rect_lines=y_right_rect_lines,
+                                  left_rect_lines=y_left_rect_lines,
                                   init_texts=y_init_texts,
                                   control_texts=y_control_texts,
                                   y_axis_centers=y_y_axis_centers,
                                   bounds=y_bounds,
                                   initial_steps=initial_steps,
+                                  continuous_updates=continuous_updates,
                                   legend_loc='lower right')
     
     # Get initial text bounding box width
@@ -658,6 +668,7 @@ def plot_input_output_animation(
         text_object=u_init_texts[0], axis=axs_u[0], fig=fig)
     init_text_width_output = get_text_width_in_data(
         text_object=y_init_texts[0], axis=axs_y[0], fig=fig)
+    
     # Calculate maximum text width between input and
     # output labels to show them at the same time
     init_text_width = max(init_text_width_input, init_text_width_output)
@@ -674,15 +685,21 @@ def plot_input_output_animation(
 
         # Update input plot data
         for i in range(m):
+            # Get lower boundary line of the initial measurement region
+            # if continuous updates are enabled
+            u_left_rect_line = (u_left_rect_lines[i]
+                                if continuous_updates else None)
             update_data_animation(index=current_index,
                                   data=u_k[:current_index + 1, i],
                                   data_length=T,
                                   points_per_frame=points_per_frame,
                                   initial_steps=initial_steps,
+                                  continuous_updates=continuous_updates,
                                   line=u_lines[i],
                                   rect=u_rects[i],
                                   y_axis_center=u_y_axis_centers[i],
-                                  rect_line=u_rect_lines[i],
+                                  right_rect_line=u_right_rect_lines[i],
+                                  left_rect_line=u_left_rect_line,
                                   init_text_obj=u_init_texts[i],
                                   control_text_obj=u_control_texts[i],
                                   display_initial_text=display_initial_text,
@@ -692,15 +709,21 @@ def plot_input_output_animation(
         
         # Update output plot data
         for j in range(p):
+            # Get lower boundary line of the initial measurement region
+            # if continuous updates are enabled
+            y_left_rect_line = (y_left_rect_lines[j]
+                                if continuous_updates else None)
             update_data_animation(index=current_index,
                                   data=y_k[:current_index + 1, j],
                                   data_length=T,
                                   points_per_frame=points_per_frame,
                                   initial_steps=initial_steps,
+                                  continuous_updates=continuous_updates,
                                   line=y_lines[j],
                                   rect=y_rects[j],
                                   y_axis_center=y_y_axis_centers[j],
-                                  rect_line=y_rect_lines[j],
+                                  right_rect_line=y_right_rect_lines[j],
+                                  left_rect_line=y_left_rect_line,
                                   init_text_obj=y_init_texts[j],
                                   control_text_obj=y_control_texts[j],
                                   display_initial_text=display_initial_text,
@@ -708,9 +731,10 @@ def plot_input_output_animation(
                                   init_text_width=init_text_width,
                                   control_text_width=control_text_width)
 
-        return (u_lines + y_lines + u_rects + u_rect_lines +
-                u_init_texts + u_control_texts + y_rects +
-                y_init_texts + y_control_texts + y_rect_lines)
+        return (u_lines + y_lines + u_rects + u_right_rect_lines +
+                u_left_rect_lines + u_init_texts + u_control_texts + y_rects +
+                y_init_texts + y_control_texts + y_right_rect_lines +
+                y_left_rect_lines)
 
     # Calculate the number of animation frames
     n_frames = math.ceil((T - 1) / points_per_frame)  + 1
@@ -737,12 +761,14 @@ def initialize_data_animation(
     legend_params: dict[str, Any],
     lines: List[Line2D],
     rects: List[Rectangle],
-    rect_lines: List[Line2D],
+    right_rect_lines: List[Line2D],
+    left_rect_lines: List[Line2D],
     init_texts: List[Text],
     control_texts: List[Text],
     y_axis_centers: List[float],
     bounds: Optional[Tuple[float, float]] = None,
     initial_steps: Optional[int] = None,
+    continuous_updates: bool = False,
     legend_loc: str = 'best'
 ) -> None:
     """
@@ -788,9 +814,12 @@ def initialize_data_animation(
             be stored.
         rects (List[Rectangle]): The list where the initialized rectangles
             representing the initial measurement region will be stored.
-        rect_lines (List[Line2D]): The list where the initialized vertical
-            lines representing the initial measurement region limit will be
-            stored.
+        right_rect_lines (List[Line2D]): The list where the initialized
+            vertical lines representing the upper boundary of the initial
+            measurement region will be stored.
+        left_rect_lines (List[Line2D]): The list where the initialized
+            vertical lines representing the lower boundary of the initial
+            measurement region will be stored.
         init_texts (List[Text]): The list where the initialized initial
             measurement label texts will be stored.
         control_texts (List[Text]): The list where the initialized control
@@ -806,14 +835,17 @@ def initialize_data_animation(
             characterization of the system. This will highlight the initial
             measurement period in the plot. If `None`, no special highlighting
             will be applied.
+        continuous_updates (bool): Whether the initial measurement period
+            highlight should move with the latest data to represent continuous
+            input-output measurement updates.
         legend_loc (str): The location of the legend on the plot. Corresponds
             to Matplotlib's `loc` parameter for legends. Defaults to 'best'.
     
     Note:
-        This function updates the `lines`, `rects`, `rect_lines`,
-        `init_texts`, and `control_texts` with the initialized plot elements.
-        It also adjusts the y-axis limits to a fixed range and stores the
-        center values in `y_axis_centers`.
+        This function updates the `lines`, `rects`, `right_rect_lines`,
+        `left_rect_lines` `init_texts`, and `control_texts` with the
+        initialized plot elements. It also adjusts the y-axis limits to a
+        fixed range and stores the center values in `y_axis_centers`.
     """
     T = data.shape[0]  # Data length
 
@@ -837,6 +869,7 @@ def initialize_data_animation(
     axis.plot(range(0, T), np.full(T, setpoint),
               **setpoint_line_params,
               label=f'${var_symbol}_{index + 1}^s$')
+
     # Format labels, legend and ticks
     axis.set_xlabel('Time step $k$', fontsize=fontsize)
     axis.set_ylabel(f'{var_label} ${var_symbol}_{index + 1}$', fontsize=fontsize)
@@ -853,15 +886,23 @@ def initialize_data_animation(
     if initial_steps:
         # Initialize initial measurement rectangle
         rects.append(axis.axvspan(0, 0, color='gray', alpha=0.1))
-        # Initialize initial measurement rectangle limit line
-        rect_lines.append(axis.axvline(
+       
+        # Initialize initial measurement rectangle boundary lines
+        right_rect_lines.append(axis.axvline(
             x=0, color='black', linestyle=(0, (5, 5)), linewidth=1))
+
+        if continuous_updates:
+            # Add left boundary line to show continuous updates, if enabled
+            left_rect_lines.append(axis.axvline(
+                x=0, color='black', linestyle=(0, (5, 5)), linewidth=1))
+    
         # Initialize initial measurement text
         init_texts.append(axis.text(
             initial_steps / 2, y_axis_centers[index],
             initial_text, fontsize=fontsize - 1, ha='center',
             va='center', color='black', bbox=dict(facecolor='white',
                                                     edgecolor='black')))
+
         # Initialize control text
         control_texts.append(axis.text(
             (T + initial_steps) / 2, y_axis_centers[index],
@@ -875,10 +916,12 @@ def update_data_animation(
     data_length: int,
     points_per_frame: int,
     initial_steps: Optional[int],
+    continuous_updates: bool,
     line: Line2D,
     rect: Rectangle,
     y_axis_center: float,
-    rect_line: Line2D,
+    right_rect_line: Line2D,
+    left_rect_line: Optional[Line2D],
     init_text_obj: Text,
     control_text_obj: Text,
     display_initial_text: bool,
@@ -906,12 +949,17 @@ def update_data_animation(
             input-output measurements were taken for the data-driven
             characterization of the system. This will highlight the initial
             measurement period in the plot.
+        continuous_updates (bool): Whether the initial measurement period
+            highlight should move with the latest data to represent continuous
+            input-output measurement updates.
         line (Line2D): The plot line corresponding to the data series plot.
         rect (Rectangle): The rectangle representing the initial measurement
             region.
         y_axis_center (float): The y-axis center of the plot axis.
-        rect_line (Line2D): The line object representing the initial
-            measurement region limit.
+        right_rect_line (Line2D): The line object representing the upper
+            boundary of the initial measurement region.
+        left_rect_line (Optional[Line2D]): The line object representing the
+            lower boundary of the initial measurement region.
         init_text_obj (Text): The text object containing the initial
             measurement period label.
         control_text_obj (Text): The text object containing the control period
@@ -928,21 +976,43 @@ def update_data_animation(
     # Update plot line data
     line.set_data(range(0, index + 1), data[:index + 1])
     
+    # Determine if an update is needed. Always update for continuous updates
+    needs_update = (index <= initial_steps + points_per_frame or
+                    continuous_updates)
+    
     # Update initial measurement rectangle and texts
-    if initial_steps and index <= initial_steps + points_per_frame:
+    if initial_steps and needs_update:
         # Calculate measurement period limit index
-        lim_index = index if index <= initial_steps else initial_steps
+        lim_index = (min(index, initial_steps)
+                     if not continuous_updates else index)
+        
         # Update rectangle width
-        rect.set_width(lim_index)
+        rect_width = min(lim_index, initial_steps)
+        rect.set_width(rect_width)
+        
+        # Update rectangle position
+        left_index = max(lim_index - initial_steps, 0)
+        rect.set_xy((left_index, 0))
+        
+        # Update rectangle boundary line positions
+        right_rect_line.set_xdata([lim_index])
+
+        if continuous_updates and left_rect_line:
+            # Update left boundary line if continuous updates are enabled
+            left_rect_line.set_xdata([left_index])
+
+            # Toggle visibility based on its index
+            left_rect_line.set_visible(left_index != 0)
+        
         # Hide initial measurement and control texts
         init_text_obj.set_visible(False)
         control_text_obj.set_visible(False)
-        # Update rectangle limit line position
-        rect_line.set_xdata([lim_index])
+
         # Show initial measurement text
         if display_initial_text and index >= init_text_width:
             init_text_obj.set_position((lim_index / 2, y_axis_center))
             init_text_obj.set_visible(True)
+        
         # Show control text if possible
         if display_control_text and index >= initial_steps:
             if (data_length - initial_steps) >= control_text_width:
