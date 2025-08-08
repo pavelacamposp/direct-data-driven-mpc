0ea6e4d0e58468c7eebbe405abf3ecb9ff09f054
Pável A. Campos-Peña <pcamposp@uni.pe>
2025-08-08 04:20:05 -0500

    Add sitemap and GSC verification file to GitHub Pages site (#42)
    * Add Sphinx sitemap configuration for GitHub Pages
    
    - Update `docs/source/conf.py` to include GitHub Pages site
      configuration for building `sitemap.xml` files.
    - Add `sphinx-sitemap` to `docs/requirements.txt`.
    
    * Add Google Search Console HTML file for verification
    
    - Add `docs/source/googlec911518ad9a1d2fa.html` for site ownership
      verification.
    - Update `docs/source/conf.py` to include this HTML file in the
      `html_extra_path` list to deploy it automatically on each Sphinx
      build.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx_copybutton",
     "sphinx_design",
+    "sphinx_sitemap",
 ]
 
 templates_path = ["_templates"]
@@ -88,6 +89,9 @@ html_favicon = "_static/favicon.png"
 html_last_updated_fmt = ""
 html_static_path = ["_static"]
 html_css_files = ["styles.css"]
+html_baseurl = "https://pavelacamposp.github.io/direct-data-driven-mpc/"
+sitemap_url_scheme = "{link}"
+html_extra_path = ["googlec911518ad9a1d2fa.html"]
 
 html_context = {
     "display_version": True,
